6602d27e25fca5bb430b1a8712aadfd6bd6f1a05
Ian Bell <ian.bell@nist.gov>
2017-01-31 14:14:51 -0700

    Rewrite timing script

--- test_pybind11.py
+++ test_pybind11.py
@@ -1,57 +1,59 @@
-import sys; sys.path.append('build/pybind11/Debug')
+import sys; sys.path.append('build/pybind11/Release')
 import PolyFitter as pf, numpy as np, time
 import matplotlib.pyplot as plt
 
-def polynomial():
-    x = np.linspace(0,1,10000)
+def get_eval_poly(Npoints):
+    x = np.linspace(0,1,Npoints)
     y = 1 + 2*x + 3*x**2 + 4*x**6
     order = 6
     outputs = [pf.PolynomialOutput(order, pf.NumericInput(_x, _y)) for _x,_y in zip(x, y)]
     eva = pf.NumericEvaluator()
     eva.add_outputs(outputs)
+    return eva, [1.5]*(order+1)
 
-    tic = time.clock()
-    o = pf.LevenbergMarquardtOptions()
-    o.c0 = [1.5]*(order+1)
-    o.threading = True
-    o.Nthreads = 4
-
-    tic = time.clock()
-    assert(len(o.c0) == order+1)
-    cfinal = pf.LevenbergMarquardt(eva, o)
-    toc = time.clock()
-    print cfinal
-    print toc-tic
-
-def decaying_exponential():
-
+def get_eval_decaying_exponential(Norder):
     a = 0.2; b = 3; c = 1.3;
-    x = np.linspace(0, 2, 500)
+    x = np.linspace(0, 2, 1000)
     y = np.exp(-a*x)*np.sin(b*x)*np.cos(c*x)
-    N = -1 # order of Taylor series
-    outputs = [pf.DecayingExponentialOutput(N, pf.NumericInput(_x, _y)) for _x,_y in zip(x, y)]
+    outputs = [pf.DecayingExponentialOutput(Norder, pf.NumericInput(_x, _y)) for _x,_y in zip(x, y)]
     eva = pf.NumericEvaluator()
     eva.add_outputs(outputs)
+    return eva, [0.5, 2, 0.8]
 
-    tic = time.clock()
+def speedtest(get_eva, args, ofname):
+
     o = pf.LevenbergMarquardtOptions()
-    o.c0 = [0.5, 2, 0.8]
-    o.threading = True
-    o.Nthreads = 4
     o.tau0 = 1
+    
+    for arg in args: # order of Taylor series expansion
+        
+        # Serial evaluation
+        eva, o.c0 = get_eva(arg)
+        tic = time.clock()
+        o.threading = False
+        cfinal = pf.LevenbergMarquardt(eva, o)
+        toc = time.clock()
+        time_serial = toc-tic
 
-    tic = time.clock()
-    cfinal = pf.LevenbergMarquardt(eva, o)
-    toc = time.clock()
-    print cfinal
-    print toc-tic
+        # Parallel evaluation
+        o.threading = True
+        times = []
+        for Nthreads in [1,2,3,4,5,6,7,8]:
+            eva, o.c0 = get_eva(arg)
+            o.Nthreads = Nthreads
+            tic = time.clock()
+            cfinal = pf.LevenbergMarquardt(eva, o)
+            toc = time.clock()
+            times.append(toc-tic)
+        plt.plot(range(1, len(times)+1),time_serial/np.array(times),label='N: '+str(arg))
 
-    plt.plot(x,y)
-    a,b,c = cfinal
-    yfit = np.exp(-a*x)*np.sin(b*x)*np.cos(c*x)
-    plt.plot(x,yfit)
+    plt.legend(loc='best')
+    plt.plot([1,8],[1,8],lw=3)
+    plt.xlabel(r'$N_{\rm threads}$ (-)')
+    plt.ylabel(r'Speedup $t_{\rm serial}/t_{\rm parallel}$ (-)')
+    plt.savefig(ofname)
     plt.show()
 
 if __name__=='__main__':
-    polynomial()
-    decaying_exponential()
+    speedtest(get_eval_poly, [100,1000,10000],'speedup_polynomial.pdf')
+    speedtest(get_eval_decaying_exponential, [10,20,30,40,50], 'speedup_decaying_exponential.pdf')
